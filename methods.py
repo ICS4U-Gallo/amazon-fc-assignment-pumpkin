238c71a8ee0271f291ab864bafc70a56b07b66a4
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2019-10-07 20:10:12 -0400

    Update methods.py

--- methods.py
+++ methods.py
@@ -1,1 +1,26 @@
-w
+def create_product_dict(product_id: str, product_name: str, product_dimensions: list) -> dict:
+    """Creates a dictionary to store a specific products information
+    
+    Args:
+        Product_id, as a string ie: 0000193
+        Product_name, as a string ie: Gillet Razor
+        Product_dimensions, a list of form: [length, width, height]
+        
+    Returns:
+        Dictonary
+        """
+    pass
+
+def scan_product_into_shelf(product_dictionary: dict, shelf_obj: object) -> :
+    
+def manage_shelf_at_employee(): pass
+
+def display_product_compartment(): pass
+
+def remove_product_from_shelf(): pass
+
+def send_to_packaging(): pass
+
+def display_pacakge_requirments(): pass
+
+def send_to_truck(): pass
