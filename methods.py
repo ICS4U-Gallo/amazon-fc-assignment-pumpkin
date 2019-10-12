db5289cc6cb746d9f4beee08a812563dec1d3b5d
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2019-10-12 15:06:34 -0400

    Update methods.py

--- methods.py
+++ methods.py
@@ -1,6 +1,20 @@
 from typing import List
+import random
 
-def create_product_dict(product_id: str, product_name: str, product_dimensions: list) -> dict:
+
+class Shelf:
+
+    def __init__(self):
+
+        items = []
+
+
+def init_shelfs(shelf_list, batch_size):
+
+    for _ in range(batch_size):
+        shellf_list.append(Shelf())
+
+def create_product_dict(product_id: str, product_name: str, product_dimensions: list) -> dict: # Joe
     """Creates a dictionary to store a specific products information
     
     Args:
@@ -13,13 +27,24 @@ def create_product_dict(product_id: str, product_name: str, product_dimensions: 
         """
     pass
 
-def scan_product_into_shelf(product_dictionary: dict, shelf_obj: object) -> None:
-    """Adds a product dictionary to a shelf_obj
+def pull_product_from_csv(row: int, file: object):
+    """From a local csv read a single product information row, and return it's information.
 
+    Args:
+        file, a file object of format csv
+        row, the row of the desired product
+
+    Returns:
+        a batch return of....... product_id, product_name, product_dimensions
+        """
+
+    pass
+
+def scan_product_into_shelf(product_dictionary: dict, shelf_obj: object) -> None: # Joe
+    """Adds a product dictionary to a shelf_obj
     Args:
         product_dictionary, a dictionary in the form of the dict created in create_product_dict
         shelf_obj, the shelf class-object add to the list of items via shelf_obj.items.append(______)
-
     Returns:
         None
         """
@@ -28,11 +53,9 @@ def scan_product_into_shelf(product_dictionary: dict, shelf_obj: object) -> None
 
 def manage_shelf_at_employee(target_id: str, shelf_obj: object) -> dict:
     """Search through the queue of customer orders, and compare it against the current availble shelf (shelf_obj.items)
-
     Args:
         target_id, a string that corosponds to a product dictionary key (ie: product_dictionary["Product_id"])
         shelf_obj, the shelf object that contains the item list for the current shelf at the employee station
-
     Returns:
         product_dict, the current employee target product that should be taken out
         """
@@ -41,7 +64,6 @@ def manage_shelf_at_employee(target_id: str, shelf_obj: object) -> dict:
 
 def display_product_compartment(target_product: dict, shelf_obj: object) -> int:
     """Notifiy the employee what compartment the target item is contained within
-
     Args:
         target_product, the dictionary that will be used to search, and ultimetly notifiy the employee of the appropriate location
         shelf_obj, the object that contains the shelf_obj.items list that will be searched
@@ -54,11 +76,9 @@ def display_product_compartment(target_product: dict, shelf_obj: object) -> int:
 
 def remove_product_from_shelf(target_product:dict, shelf_obj: object) -> None:
     """After an employee is told to take a product out of a shelf, it has to be removed from the list as it is no longer physically there
-
     Args:
         target_product, a dictionary that corroponds to the item that should be removed.
         shelf_obj, the class-object that contains the shelf_obj.items list that needs to be removed from.
-
     Returns:
         None
         """
@@ -67,7 +87,6 @@ def remove_product_from_shelf(target_product:dict, shelf_obj: object) -> None:
 
 def send_to_packaging(product_dict: dict, order_info: dict) -> None:
     """After a product is removed from the shelf it needs to be packaged and shipped.
-
     Args:
         product_dict, a dictionary that contains the information about the product
         order_info, a dictionary that contains the information about the order
@@ -78,5 +97,19 @@ def send_to_packaging(product_dict: dict, order_info: dict) -> None:
     
     pass
 
-def display_pacakge_requirments(product_dict: dict, order_info: dict) -> None: pass 
-    # Ignore this function for now....
+orders = []
+shelf_obj_list = []
+
+init_shelfs(1000)
+
+while True:
+
+    for _ in range(random.randint(0,5)): # Each iteration 0-5 random products will be ordered
+
+        with open("products.csv") as csv_file:
+
+            id, name, dimensions = pull_product_from_csv(random.randint(0, len(csv_file)), csv_file) # Randomly select a product to order
+
+            product_dict = create_product_dict(id, name, dimensions)
+
+            scan_product_into_shelf(product_dict, shelf_obj_list[random.randint(0,len(shelf_obj_list))]) # Add the current product into a randomly selected shelf
