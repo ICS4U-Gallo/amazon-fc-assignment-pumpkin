304d062b5bad1a0f39f05ea8b2415cbbb4bb8e92
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2019-10-14 11:08:06 -0400

    Update methods.py

--- methods.py
+++ methods.py
@@ -14,28 +14,15 @@ def init_shelfs(shelf_list, batch_size):
     for _ in range(batch_size):
         shellf_list.append(Shelf())
 
-def create_product_dict(product_id: str, product_name: str, product_dimensions: list) -> dict: # Joe
-    """Creates a dictionary to store a specific products information
-    
-    Args:
-        Product_id, as a string ie: 0000193
-        Product_name, as a string ie: Gillet Razor
-        Product_dimensions, a list of form: [length, width, height]
-        
-    Returns:
-        Dictonary
-        """
-    pass
-
-def pull_product_from_csv(row: int, file: object):
-    """From a local csv read a single product information row, and return it's information.
+def pull_product_from_json(id: str, file: dict):
+    """From a local json read a single product information key, and return it's information.
 
     Args:
-        file, a file object of format csv
-        row, the row of the desired product
+        file, a file object of format json
+        id, the key of the desired product
 
     Returns:
-        a batch return of....... product_id, product_name, product_dimensions
+        a dict of: product_id, product_name, product_dimensions; key value pairs
         """
 
     pass
@@ -97,15 +84,6 @@ def send_to_packaging(product_dict: dict, order_info: dict) -> None:
     
     pass
 
-def generate_random_orders(products_filename, orders_list):
-
-    with open(products_filename) as csv_file:
-
-        id, name, dimensions = pull_product_from_csv(random.randint(0, len(csv_file)), csv_file) # Randomly select a product to order
-        product_dict = create_product_dict(id, name, dimensions)
-
-        orders_list.append(product_dict)
-
 orders = []
 shelf_obj_list = []
 
@@ -117,10 +95,4 @@ shelf_unloading_stations = 5
 order_packaging_stations = 5
 
 while True:
-
-    for _ in range(random.randint(0,5)): # 0-5 random products will be ordered
-        generate_random_orders("products.csv", orders)
-
-    
-    for _ in range(shelf_loading_stations):
-        scan_product_into_shelf(product_dict, shelf_obj_list[random.randint(0,len(shelf_obj_list))]) # Add the current product into a randomly selected shelf
+   break
