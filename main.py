7006fae15afd421749cd2a12e830f46de317e555
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2019-11-01 06:30:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from sessions import *
 from methods import *
+from sys import argv
+
+def main(input):
+
+    del input[0]
+    session_obj = Session(input, "products.csv")
+    
+    if session_obj.type == "scan_in":
+        user_obj = Scan_in(session_obj)
+
+    elif session_obj.type == "scan_out":
+        user_obj = Scan_out(session_obj)
+
+    else:
+        user_obj = Packager(session_obj)
+
+    while True:
+        user_obj.action()
 
 def initialize_session(_type_):
     if _type_ == "scan_in":
@@ -10,3 +28,17 @@ def initialize_session(_type_):
 
     if _type_ == "packager":
         packager_session()
+
+if "-h" in argv:
+    print("""Positional arguments include
+    Worker-Type (scan-in, scan-out, packager)
+    Product-Flow (capped at 200)
+    Order-Flow (capped at 200)
+    Flow-Cap (off, to remove the natural limit of 200 simulated products and orders)
+    
+    Example: python3 main.py scan-in 50 50
+    
+    Example: python3 main.py scan-in 201 201 off""")
+
+elif __name__ == "__main__":
+    main(argv)
