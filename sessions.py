1cbb51fd7d931586c92474727693073a47a79b4e
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2019-11-01 06:31:05 -0400

    Update sessions.py

---
+++ sessions.py
@@ -0,0 +1,123 @@
+from methods import *
+import sys
+
+class Session:
+
+    def __init__(self, init_args, product_csv_filename):
+        
+        self.prod_filename = product_csv_filename
+
+        self.appropriate_args = [
+            ["scan-in","scan-out","packager"],
+            [list(range(200))],
+            [list(range(200))],
+            ["off"]
+        ]
+
+        #Checking the given arguments for correctness
+        
+        if len(init_args) < 3:
+            print("You have not entered the correct fields, type python3 main.py -h")
+            sys.exit()
+
+        if len(init_args) == 3:
+            
+            try:
+                init_args[1] = int(init_args[1])
+                init_args[2] = int(init_args[2])
+
+            except:
+                    print("You have not entered the correct fields, type python3 main.py -h")
+                    sys.exit()
+
+            for i in range(len(init_args)):
+                if init_args[i] not in self.appropriate_args[i]:
+                    print("You have not entered the correct fields, type python3 main.py -h")
+                    sys.exit()
+            
+            else:
+                self.type = init_args[0]
+                
+                if init_args[1] < 200:
+                    self.products = init_args[1]
+                else:
+                    self.products = 200
+                
+                if init_args[2] < 200:
+                    self.orders = init_args[2]
+                else:
+                    self.orders = 200
+
+        if len(init_args) == 4:
+
+            try:
+                init_args[1] = int(init_args[1])
+                init_args[2] = int(init_args[2])
+
+            except:
+                    print("You have not entered the correct fields, type python3 main.py -h")
+                    sys.exit()
+
+            for i in range(len(init_args)):
+                if init_args[i] not in self.appropriate_args[i] and init_args[2]:
+                    print("You have not entered the correct fields, type python3 main.py -h")
+                    sys.exit()
+            
+            else:
+                self.type = init_args[0]
+                self.products = init_args[1]
+                self.orders = init_args[2]
+
+        if len(init_args) > 4:
+            print("You have not enterd the correct fields, type python3 main.py -h")
+            sys.exit()
+
+
+class Scan_in:
+    def __init__(self, session_obj):
+
+        self.sess = session_obj
+
+        self.inbound_shipments = []
+        generate_random_shipment(self.sess.products, self.inbound_shipments, self.sess.prod_filename)
+
+        self.shelves = []
+        generate_empty_shelves(100, self.shelves)
+
+    def update(self):
+        generate_random_shipment(self.sess.products, self.inbound_shipments, self.sess.prod_filename)
+
+    def action(self):
+        pass
+
+class Scan_out:
+
+    def __init__(self, session_obj):
+
+        self.sess = session_obj
+
+        self.to_be_packaged = []
+        generate_random_order(self.to_be_packaged, 10, self.sess.prod_filename)
+
+        self.shelves = []
+        generate_empty_shelves(100, self.shelves)
+
+        fill_shelves_randomly(self.shelves, self.sess.prod_filename,2)
+
+    def update(self):
+        fill_shelves_randomly(self.shelves,self.sess.prod_filename,1)
+
+    def action(self):
+        pass
+
+class Packager:
+
+    def __init__(self, session_obj):
+
+        self.sess = session_obj
+
+        self.completed_parcels = []
+
+        self.inbound_trolley = []
+
+        generate_random_shipment(self.sess.products, self.inbound_trolley, )
